5f357ee9ca4dcb91f517104607e6ebf3b0e46570
Filippo Lampa <56674164+filippo-lampa@users.noreply.github.com>
2023-10-17 01:07:25 +0000

    [add] architecture to fine-tuned the ASR model

--- ASR.py
+++ ASR.py
@@ -1,8 +1,10 @@
 from transformers import pipeline
 from Dataset_Manager import Dataset_Manager
+import torch
 
 if __name__ == '__main__':
-    asr = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-100h")
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    asr = pipeline("automatic-speech-recognition", model="openai/whisper-base", device=device)
     dataset = Dataset_Manager().get_dataset()
-    result = asr(dataset['train'][6]['audio'].copy())
+    result = asr(dataset['train'][1]['audio'].copy(), generate_kwargs={"task": "transcribe"})
     print(result)
